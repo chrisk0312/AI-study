f2c0050a502c820a4bb5d68da00fb5f812c0344e
chrisk0312 <kangjt0312@gmail.com>
2024-01-03 18:32:42 +0900

    date0103

--- keras/keras06_train_test1.py
+++ keras/keras06_train_test1.py
@@ -3,20 +3,14 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 #1.데이터
-x = np.array([1,2,3,4,5,6,7,8,9,10])
-y = np.array([1,2,3,4,6,5,7,8,9,10])
+#x = np.array([1,2,3,4,5,6,7,8,9,10])
+#y = np.array([1,2,3,4,6,,5,7,8,9,10])
 
-#[실습] 넘파이 리스트의 슬라이싱! 7:3
-x_train = x[:7]
-y_train = y[:7]
+x_train = np.array([1,2,3,4,5,6,7])
+y_train = np.array([1,2,3,4,6,5,7])
 
-x_test = x[:7]
-y_test = y[:7]
-
-print(x_train)
-print(y_train)
-print(x_test)
-print(x_test)
+x_test = np.array([8,9,10])
+y_test = np.array([8,9,10])
 
 #2. 모델구성
 model = Sequential()
@@ -32,6 +26,6 @@ model.fit(x_train,y_train, epochs=300, batch_size =1)
 
 #4. 평가,예측
 loss = model.evaluate(x_test,y_test)
-results = model.predict([1])
+results = model.predict([11000,7])
 print("로스 :", loss)
-print("[1]의 예측값 :", results)
+print("[11000]의 예측값 :", results)
--- keras/keras06_train_test2.py
+++ keras/keras06_train_test2.py
@@ -3,14 +3,20 @@ from keras.models import Sequential
 from keras.layers import Dense
 
 #1.데이터
-#x = np.array([1,2,3,4,5,6,7,8,9,10])
-#y = np.array([1,2,3,4,6,,5,7,8,9,10])
+x = np.array([1,2,3,4,5,6,7,8,9,10])
+y = np.array([1,2,3,4,6,5,7,8,9,10])
 
-x_train = np.array([1,2,3,4,5,6,7])
-y_train = np.array([1,2,3,4,6,5,7])
+#[실습] 넘파이 리스트의 슬라이싱! 7:3
+x_train = x[:7]
+y_train = y[:7]
 
-x_test = np.array([8,9,10])
-y_test = np.array([8,9,10])
+x_test = x[:7]
+y_test = y[:7]
+
+print(x_train)
+print(y_train)
+print(x_test)
+print(x_test)
 
 #2. 모델구성
 model = Sequential()
@@ -26,6 +32,6 @@ model.fit(x_train,y_train, epochs=300, batch_size =1)
 
 #4. 평가,예측
 loss = model.evaluate(x_test,y_test)
-results = model.predict([11000,7])
+results = model.predict([1])
 print("로스 :", loss)
-print("[11000]의 예측값 :", results)
+print("[1]의 예측값 :", results)
