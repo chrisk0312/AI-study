8aff628bc9540ced86c54a3ceeed640ef8881d34
chrisk0312 <kangjt0312@gmail.com>
2024-01-04 09:34:59 +0900

    0104

--- keras/keras06_train_test1.py
+++ keras/keras06_train_test1.py
@@ -4,7 +4,7 @@ from keras.layers import Dense
 
 #1.데이터
 #x = np.array([1,2,3,4,5,6,7,8,9,10])
-#y = np.array([1,2,3,4,6,,5,7,8,9,10])
+#y = np.array([1,2,3,4,6,5,7,8,9,10])
 
 x_train = np.array([1,2,3,4,5,6,7])
 y_train = np.array([1,2,3,4,6,5,7])
