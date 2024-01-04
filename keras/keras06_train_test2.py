36bca0b6d734a6870ae1d62d7fa687d23536ac07
chrisk0312 <kangjt0312@gmail.com>
2024-01-04 14:22:13 +0900

    Jan 04

--- keras/keras06_train_test2.py
+++ keras/keras06_train_test2.py
@@ -7,11 +7,14 @@ x = np.array([1,2,3,4,5,6,7,8,9,10])
 y = np.array([1,2,3,4,6,5,7,8,9,10])
 
 #[실습] 넘파이 리스트의 슬라이싱! 7:3
-x_train = x[:7]
+x_train = x[:7] # [0:7] == [:-3] == [0:-3]
 y_train = y[:7]
-
-x_test = x[:7]
-y_test = y[:7]
+'''
+a = b # a라는 변수에  b 값을 넣어라
+a == b # a와 b가 같다
+'''
+x_test = x[7:] #[7:10] == [=3:] == [-3:10]
+y_test = y[7:]
 
 print(x_train)
 print(y_train)
@@ -26,7 +29,7 @@ model.add(Dense(10))
 model.add(Dense(13))
 model.add(Dense(1))
 
-#3.
+#3. 컴파일,훈련
 model.compile(loss='mse',optimizer='adam')
 model.fit(x_train,y_train, epochs=300, batch_size =1)
 
