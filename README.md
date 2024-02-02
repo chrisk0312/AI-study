acb5805c31d6f8f94fe9214e82b4afe078f11387
chrisk0312 <kangjt0312@gmail.com>
2024-02-02 12:40:38 +0900

    readme.md

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # JeongTae Kang
 ## Korean(native), English(navtive)
-## Education Background [korea, Australia(The Knox School),Canada(Seneca College)]
+## Education Background [South Korea, Australia(The Knox School),Canada(Seneca College)]
 ## 1. Seneca College (Aviation Operation) _ Graduated
 ## 2. Apple Korea Subcontract System Engineer _ Aug/2022 ~ Nov/2023
 ## Apple Certified Support Professional_(https://www.credly.com/badges/84bf1596-e77c-461d-a4a8-d022b1000d6a/public_url)
--- keras/keras31_cnn.py
+++ keras/keras31_cnn.py
@@ -9,3 +9,10 @@ model.add(Conv2D(10,(2,2), input_shape=(10,10,1)))
 model.add(Conv2D(5,(2,2),input_shape=(9,9,10)))
 model.add(Dense(5))
 model.add(Dense(1))
+
+# A Convolutional Neural Network (CNN) is a type of artificial neural network 
+# that is especially effective for processing structured grid data, such as images. 
+# CNNs are used for image classification, object detection, video processing, and even in natural language processing.
+# They have their "neurons" arranged in three dimensions: width, height, and depth. 
+# The neurons in a layer are only connected to a small region of the layer before it, called the receptive field.
+
