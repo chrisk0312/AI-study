cc0b03c7ea84f06476bba5d38a9f919c5490b771
chrisk0312 <kangjt0312@gmail.com>
2024-02-08 14:40:08 +0900

    Feb08

--- machine learning/m05_01_kfold_iris.py
+++ machine learning/m05_01_kfold_iris.py
@@ -2,12 +2,13 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.model_selection import StratifiedKFold
 
 #1. data
 x,y = load_iris(return_X_y=True)
 
 n_splits = 5
-kfold = KFold(n_splits=n_splits, shuffle=True, random_state=123)
+kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=123)
 
 #2. modeling
 model = SVC()
