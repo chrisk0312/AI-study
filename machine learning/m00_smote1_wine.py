07f14949ad022e5fdcb767fe64841b7f701b5b66
chrisk0312 <kangjt0312@gmail.com>
2024-02-14 20:06:29 +0900

    Feb14

--- machine learning/m00_smote1_wine.py
+++ machine learning/m00_smote1_wine.py
@@ -5,7 +5,7 @@ from keras.layers import Dense
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, f1_score
-
+from imblearn.over_sampling import SMOTE
 #1
 datasets = load_wine()
 x = datasets.data
@@ -25,44 +25,19 @@ x_train, x_test, y_train, y_test = train_test_split(
     x,y, train_size=0.75, shuffle= True, random_state=123,
     stratify=y, 
 )
+# SMOTE stands for Synthetic Minority Over-sampling Technique. 
+# It's a technique used to handle imbalanced datasets, where one class of data is significantly less frequent than the other(s).
+# In an imbalanced dataset, traditional machine learning algorithms can be biased towards the majority class,
+# leading to poor performance on the minority class. SMOTE helps to overcome this issue by creating synthetic (not duplicate) samples of the minority class.
+# For each instance in the minority class, SMOTE calculates the k-nearest neighbors. The parameter k is user-defined.
+# Depending on the amount of oversampling required, one or more of the k-nearest neighbors are selected to create the synthetic examples.
+# Synthetic examples are created by taking the difference between 
+# the feature vector (attributes) of the instance under consideration and its selected neighbor, 
+# multiplying this difference by a random number between 0 and 1, and then adding it back to the feature vector.
 
 
-'''
-#2 
-model=Sequential()
-model.add(Dense(10, input_shape=(13,)))
-model.add(Dense(3, activation='softmax'))
-
-#3
-model.compile(loss='sparse_categorical_crossentropy', optimizer='adam',
-              metrics=['acc'])
-model.fit(x_train,y_train, epochs=50, validation_split=0.2)
-
-#4
-results = model.evaluate(x_test, y_test)
-print("loss:", results[0])
-print("acc:", results[1])
-
-y_pred = model.predict(x_test)
-print(y_test)
-print(y_pred)
-
-y_pred = np.argmax(y_pred, axis=1)
-print(y_pred)
-
-acc= accuracy_score(y_test, y_pred)
-print('acc',acc)
-f1 = f1_score(y_test, y_pred, average='weighted')
-print('f1',f1)
-
-# acc 0.8055555555555556
-# f1 0.7676470588235293
-'''
 ##################### smote ########################
 print("==================smote 적용==========================")
-from imblearn.over_sampling import SMOTE
-import sklearn as sk
-print('sklearn:', sk.__version__) #sklearn: 1.3.0
 
 smote = SMOTE(random_state=123)
 x_train, y_train = smote.fit_resample(x_train, y_train)
--- machine learning/m02_1_and.py
+++ machine learning/m02_1_and.py
@@ -10,8 +10,14 @@ y_data = np.array([0, 0, 0, 1])
 print(x_data.shape, y_data.shape) #(4, 2) (4,)
 
 #2 model
-# model = LinearSVC()
 model = Perceptron()
+
+# A perceptron is a simple machine learning algorithm that works well for binary classification tasks. 
+# It's a type of linear classifier, meaning it makes predictions based on a linear predictor function combining a set of weights with the feature vector.
+# The perceptron takes in an input, multiplies it by a weight, and then sums the results. 
+# If the sum is greater than a certain threshold, the perceptron returns 1; otherwise, it returns 0.
+# During training, the perceptron is provided with examples for which the output is known.
+# It makes predictions for these examples, and then adjusts its weights based on the errors it made.
 
 #3 fit
 model.fit(x_data, y_data)
