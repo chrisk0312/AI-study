0678dd76573ae1813a9eaac937786a2867e9b117
chrisk0312 <kangjt0312@gmail.com>
2024-02-16 19:06:11 +0900

    Feb16

--- pandas/pd04_3_loan.py
+++ pandas/pd04_3_loan.py
@@ -1,76 +1,49 @@
+#https://dacon.io/competitions/official/236214/data
+
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.metrics import f1_score
-from keras.callbacks import EarlyStopping, ModelCheckpoint
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder, MinMaxScaler
+from keras.callbacks import EarlyStopping
 import time
-from sklearn.svm import LinearSVC
-
 import warnings
 warnings.filterwarnings(action='ignore')
 
-path = "C:\\_data\\DACON\\loan\\"
 
-train_csv = pd.read_csv(path+"train.csv",index_col=0)
-test_csv = pd.read_csv(path+"test.csv",index_col=0)
-submission_csv = pd.read_csv(path+"sample_submission.csv")
+#1. 데이터
+path = "C:\\_data\\dacon\\dechul\\"
 
-# print(train_csv.shape, test_csv.shape) #(96294, 14) (64197, 13)
-# print(train_csv.columns, test_csv.columns,sep='\n',end="\n======================\n")
-# Index(['대출금액', '대출기간', '근로기간', '주택소유상태', '연간소득', '부채_대비_소득_비율', '총계좌수', '대출목적',
-#        '최근_2년간_연체_횟수', '총상환원금', '총상환이자', '총연체금액', '연체계좌수', '대출등급'],
-#       dtype='object')
-# Index(['대출금액', '대출기간', '근로기간', '주택소유상태', '연간소득', '부채_대비_소득_비율', '총계좌수', '대출목적',
-#        '최근_2년간_연체_횟수', '총상환원금', '총상환이자', '총연체금액', '연체계좌수'],
-#       dtype='object')
-
-# print(np.unique(train_csv['주택소유상태'],return_counts=True))
-# print(np.unique(test_csv['주택소유상태'],return_counts=True),end="\n======================\n")
-# (array(['ANY', 'MORTGAGE', 'OWN', 'RENT'], dtype=object), array([    1, 47934, 10654, 37705], dtype=int64))
-# (array(['MORTGAGE', 'OWN', 'RENT'], dtype=object), array([31739,  7177, 25281], dtype=int64))
-
-# print(np.unique(train_csv['대출목적'],return_counts=True))
-# print(np.unique(test_csv['대출목적'],return_counts=True),end="\n======================\n")
-# (array(['기타', '부채 통합', '소규모 사업', '신용 카드', '의료', '이사', '자동차', '재생 에너지',
-#        '주요 구매', '주택', '주택 개선', '휴가'], dtype=object), array([ 4725, 55150,   787, 24500,  1039,   506,   797,    60,  1803,
-#          301,  6160,   466], dtype=int64))
-# (array(['결혼', '기타', '부채 통합', '소규모 사업', '신용 카드', '의료', '이사', '자동차',
-#        '재생 에너지', '주요 구매', '주택', '주택 개선', '휴가'], dtype=object), array([    1,  3032, 37054,   541, 16204,   696,   362,   536,    29,
-#         1244,   185,  4019,   294], dtype=int64))
-
-# print(np.unique(train_csv['대출등급'],return_counts=True),end="\n======================\n")
-# (array(['A', 'B', 'C', 'D', 'E', 'F', 'G'], dtype=object), array([16772, 28817, 27623, 13354,  7354,  1954,   420], dtype=int64))
-
-train_csv = train_csv[train_csv['주택소유상태'] != 'ANY'] #ANY은딱 한개 존재하기에 그냥 제거
-# test_csv = test_csv[test_csv['대출목적'] != '결혼']
-test_csv.loc[test_csv['대출목적'] == '결혼' ,'대출목적'] = '기타' #결혼은 제거하면 개수가 안맞기에 기타로 대체
-
-# x.loc[x['type'] == 'red', 'type'] = 1
-# print(np.unique(train_csv['주택소유상태'],return_counts=True))
-# print(np.unique(test_csv['주택소유상태'],return_counts=True),end="\n======================\n")
-# print(np.unique(train_csv['대출목적'],return_counts=True))
-# print(np.unique(test_csv['대출목적'],return_counts=True),end="\n======================\n")
+train_csv = pd.read_csv(path + 'train.csv', index_col= 0)
+test_csv = pd.read_csv(path + 'test.csv', index_col= 0)
+submission_csv = pd.read_csv(path + 'sample_submission.csv')
 
 #대출기간 처리
-train_csv['대출기간'] = train_csv['대출기간'].replace({' 36 months' : 36 , ' 60 months' : 60 }).astype(int)
-test_csv['대출기간'] = test_csv['대출기간'].replace({' 36 months' : 36 , ' 60 months' : 60 }).astype(int)
-# train_loan_time = train_csv['대출기간']
-# train_loan_time = train_loan_time.str.split()
-# for i in range(len(train_loan_time)):
-#     train_loan_time.iloc[i] = int(train_loan_time.iloc[i][0]) #앞쪽 숫자만 따서 int로 변경
-  
-# train_csv['대출기간'] = train_loan_time 
+train_loan_time = train_csv['대출기간']
+train_loan_time = train_loan_time.str.split()
+for i in range(len(train_loan_time)):
+    train_loan_time.iloc[i] = int((train_loan_time)[i][0])
     
-# test_loan_time = test_csv['대출기간']
-# test_loan_time = test_loan_time.str.split()
-# for i in range(len(test_loan_time)):
-#     test_loan_time.iloc[i] = int(test_loan_time.iloc[i][0]) #앞쪽 숫자만 따서 int로 변경    
+#print(train_loan_time)   
 
-# test_csv['대출기간'] = test_loan_time
+test_loan_time = test_csv['대출기간']
+test_loan_time = test_loan_time.str.split()
+for i in range(len(test_loan_time)):
+    test_loan_time.iloc[i] = int((test_loan_time)[i][0])
+
+train_csv['대출기간'] = train_loan_time
+test_csv['대출기간'] = test_loan_time
+
+le = LabelEncoder()
+
+#print(test_csv)
+
+le.fit(train_csv['대출기간'])
+train_csv['대출기간'] = le.transform(train_csv['대출기간'])
+le.fit(test_csv['대출기간'])
+test_csv['대출기간'] = le.transform(test_csv['대출기간'])
 
 #근로기간 처리
 train_working_time = train_csv['근로기간']
@@ -104,147 +77,92 @@ test_working_time = test_working_time.fillna(test_working_time.mean())
 
 train_csv['근로기간'] = train_working_time
 test_csv['근로기간'] = test_working_time 
+#print(test_csv['근로기간'])
 
-#주택소유상태 처리
+le.fit(train_csv['주택소유상태'])
+train_csv['주택소유상태'] = le.transform(train_csv['주택소유상태'])
+le.fit(test_csv['주택소유상태'])
+test_csv['주택소유상태'] = le.transform(test_csv['주택소유상태'])
 
-trian_have_house = train_csv['주택소유상태']
-label_encoder = LabelEncoder()
-trian_have_house = label_encoder.fit_transform(trian_have_house)
-train_csv['주택소유상태'] = trian_have_house
+le.fit(train_csv['대출목적'])
+train_csv['대출목적'] = le.transform(train_csv['대출목적'])
+le.fit(test_csv['대출목적'])
+test_csv['대출목적'] = le.transform(test_csv['대출목적'])
 
-test_have_house = test_csv['주택소유상태']
-label_encoder = LabelEncoder()
-test_have_house = label_encoder.fit_transform(test_have_house)
-test_csv['주택소유상태'] = test_have_house
+x = train_csv.drop(['대출등급'], axis = 1)
+mms = MinMaxScaler()
+mms.fit(x)
+x = mms.transform(x)
+test_csv=mms.transform(test_csv)
 
-#대출목적 처리
-trian_loan_purpose = train_csv['대출목적']
-label_encoder = LabelEncoder()
-trian_loan_purpose = label_encoder.fit_transform(trian_loan_purpose)
-train_csv['대출목적'] = trian_loan_purpose
-
-test_loan_purpose = test_csv['대출목적']
-label_encoder = LabelEncoder()
-test_loan_purpose = label_encoder.fit_transform(test_loan_purpose)
-test_csv['대출목적'] = test_loan_purpose
-
-#대출등급 처리
-train_loan_grade = train_csv['대출등급']
-label_encoder = LabelEncoder()
-train_loan_grade = label_encoder.fit_transform(train_loan_grade)
-train_csv['대출등급'] = train_loan_grade
-
-# print(train_csv.isna().sum(),test_csv.isna().sum(), sep='\n') #결측치 제거 완료 확인함
-
-# for label in train_csv:                                       #모든 데이터가  또는 실수로 변경됨을 확인함
-#     for data in train_csv[label]:
-#         if type(data) != type(1) and type(data) != type(1.1):
-#             print("not int, not float : ",data)
-
-
-# for label in test_csv:
-#     print(label)
-#     print(f"train[{label}]: ",np.unique(train_csv[label],return_counts=True))
-#     print(f"test[{label}]",np.unique(test_csv[label],return_counts=True))
-x = train_csv.drop(['대출등급'],axis=1)
+#print(x)
 y = train_csv['대출등급']
+y = le.fit_transform(y)
 
-print(f"{test_csv.shape=}")
-print(np.unique(y,return_counts=True)) #(array([0, 1, 2, 3, 4, 5, 6]), array([16772, 28817, 27622, 13354,  7354,  1954,   420], dtype=int64))
+y= y.reshape(-1,1)
+ohe = OneHotEncoder(sparse= False)
+y = ohe.fit_transform(y)
 
-y = y.to_frame(['대출등급'])
-# y = y.reshape(-1,1)
+print(np.unique(y, return_counts= True)) #Name: 근로기간, Length: 96294, dtype: float64
 
+# Check for missing values in train_csv
+print(train_csv.isnull().sum())
+# (array([0., 1.]), array([577764,  96294], dtype=int64))
+# 대출금액            0
+# 대출기간            0
+# 근로기간            0
+# 주택소유상태          0
+# 연간소득            0
+# 부채_대비_소득_비율     0
+# 총계좌수            0
+# 대출목적            0
+# 최근_2년간_연체_횟수    0
+# 총상환원금           0
+# 총상환이자           0
+# 총연체금액           0
+# 연체계좌수           0
+# 대출등급            0
+# dtype: int64
+print('=====================')
 
-f1 = 0
+def find_outliers(train_csv):
+    # Select only numeric columns
+    numeric_cols = train_csv.select_dtypes(include=[np.number])
 
-r = int(np.random.uniform(1,1000))
+    # Calculate IQR for each column
+    Q1 = numeric_cols.quantile(0.25)
+    Q3 = numeric_cols.quantile(0.75)
+    IQR = Q3 - Q1
 
-x_train, x_test, y_train, y_test = train_test_split(x,y,train_size=0.7,random_state=r,stratify=y)
+    # Define a mask for values which are NOT outliers
+    mask = ~((numeric_cols < (Q1 - 1.5 * IQR)) | (numeric_cols > (Q3 + 1.5 * IQR)))
 
-from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, StandardScaler, RobustScaler
-# scaler = MinMaxScaler().fit(x_train)    #최솟값을 0 최댓값을 1로 스케일링
-# scaler = StandardScaler().fit(x_train)  #정규분포로 바꿔줘서 스케일링
-# scaler = MaxAbsScaler().fit(x_train)    #
-scaler = RobustScaler().fit(x_train)    #
+    # Return rows with outliers
+    return numeric_cols[~mask].dropna(how='all')
 
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
-test_csv = scaler.transform(test_csv)
+# Use the function to find outliers
+outliers = find_outliers(train_csv)
+print(outliers)
+#              대출금액  대출기간  근로기간  주택소유상태         연간소득  부채_대비_소득_비율  총계좌수  대출목적  최근_2년간_연체_횟수  총상환원
+# 금      총상환이자  총연체금액  연체계좌수
+# ID
+# TRAIN_00001   NaN   NaN   NaN     NaN          NaN          NaN   NaN  10.0           NaN    NaN        NaN    NaN    NaN
+# TRAIN_00004   NaN   NaN   NaN     NaN          NaN          NaN   NaN   8.0           NaN    NaN        NaN    NaN    NaN
+# TRAIN_00005   NaN   NaN   NaN     NaN          NaN          NaN   NaN  11.0           NaN    NaN        NaN    NaN    NaN
+# TRAIN_00006   NaN   NaN   NaN     NaN          NaN          NaN   NaN  11.0           NaN    NaN        NaN    NaN    NaN
+# TRAIN_00010   NaN   NaN   NaN     NaN          NaN          NaN   NaN   NaN           NaN    NaN  1523172.0    NaN    NaN
+# ...           ...   ...   ...     ...          ...          ...   ...   ...           ...    ...        ...    ...    ...
+# TRAIN_96286   NaN   NaN   NaN     NaN  244800000.0          NaN   NaN   NaN           NaN    NaN  2075832.0    NaN    NaN
+# TRAIN_96288   NaN   NaN   NaN     NaN          NaN          NaN   NaN   NaN          10.0    NaN        NaN    NaN    NaN
+# TRAIN_96289   NaN   NaN   NaN     NaN  210000000.0          NaN   NaN   NaN           NaN    NaN        NaN    NaN    NaN
+# TRAIN_96290   NaN   NaN   NaN     NaN          NaN          NaN   NaN  10.0           NaN    NaN        NaN    NaN    NaN
+# TRAIN_96292   NaN   NaN   NaN     NaN          NaN          NaN   NaN   NaN           2.0    NaN        NaN    NaN    NaN
 
-# scaler = MinMaxScaler().fit(x_train)    #최솟값을 0 최댓값을 1로 스케일링
-scaler = StandardScaler().fit(x_train)  #정규분포로 바꿔줘서 스케일링
-# scaler = MaxAbsScaler().fit(x_train)    #
-# scaler = RobustScaler().fit(x_train)    #
+import matplotlib.pyplot as plt
 
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
-test_csv = scaler.transform(test_csv)
+# Select only numeric columns
+numeric_cols = train_csv.select_dtypes(include=[np.number])
 
-print(f"{x_train.shape=}\n{x_test.shape=}\n{y_train.shape=}\n{y_test.shape=}")
-# x_train.shape=(67405, 13)
-# x_test.shape=(28888, 13)
-# y_train.shape=(67405, 7)
-# y_test.shape=(28888, 7)
-
-model = LinearSVC(C=900)
-# C가 작을수록 직선에 가깝고 C가 클수록 굴곡이 많다
-
-#compile & fit
-
-x_train =np.asarray(x_train).astype(np.float32) #Numpy는 기본적으로 float32 연산이기 때문에 되도록 맞춰주는게 좋다
-x_test =np.asarray(x_test).astype(np.float32)
-test_csv =np.asarray(test_csv).astype(np.float32)
-
-model.fit(x_train,y_train)
-
-#evaluate & predict
-loss = model.score(x_test, y_test)    
-y_predict = model.predict(x_test)
-y_submit = model.predict(test_csv)
-ohe_y_test = y_test
-y_test = np.argmax(y_test,axis=1)
-
-print(f"{r=}\n LOSS: {loss}")#\nF1:   {f1}")
-
-# y = y.to_frame(['대출등급'])
-# y_predict = y_predict.reshape(-1,1)
-# ohe = OneHotEncoder(sparse=False)
-# ohe_y_predict = ohe.fit_transform(y_predict)
-
-# print(ohe_y_test.shape, ohe_y_predict.shape)
-# print(np.unique(ohe_y_test),np.unique(ohe_y_predict))
-# f1 = f1_score(ohe_y_test,ohe_y_predict,average='samples')
-f1 = f1_score(y_test,y_predict,average='macro')
-print("=========================\nF1: ",f1)
-
-y_submit = label_encoder.inverse_transform(y_submit)
-
-import datetime
-dt = datetime.datetime.now()
-submission_csv['대출등급'] = y_submit
-submission_csv.to_csv(path+f"submit_{dt.day}day{dt.hour:2}{dt.minute:2}_F1{f1:.4f}.csv",index=False)
-
-
-# r=657
-#  LOSS: 1237.2230224609375
-# ACC:  0.5243353843688965
-
-# MinMaxScaler
-# F1:  0.8378815912825547
-
-# StandardScaler
-# F1:  0.8308539115878224
-
-# MaxAbsScaler
-# F1:  0.8334220011728465
-
-# RobustScaler
-# F1:  0.8429713541136693
-
-# LinearSVC
-# F1:  0.06656323877068558
-# F1:  0.06669620276531724
-# F1:  0.04583933551966162 (c=10)
-# F1:  0.06441135012563584 (C=300)
-# F1:  0.034895257354102815 (C=900)
+# Create a boxplot
+plt.boxplot(numeric_cols)
+plt.show()
