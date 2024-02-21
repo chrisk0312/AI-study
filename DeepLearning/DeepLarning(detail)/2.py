2ff679988a91b6b996ffc5234735626e2a41519f
chrisk0312 <kangjt0312@gmail.com>
2024-02-21 12:55:05 +0900

    Feb21

--- DeepLearning/DeepLarning(detail)/2.py
+++ DeepLearning/DeepLarning(detail)/2.py
@@ -35,3 +35,27 @@ print("model.compile")
 print("model.compile")
 print("model.compile")
 print("model.compile")
+
+from keras.preprocessing.image import ImageDataGenerator
+
+# create a data generator
+datagen = ImageDataGenerator(
+        rotation_range=20,     # randomly rotate images in the range (degrees, 0 to 180)
+        zoom_range = 0.1,      # Randomly zoom image 
+        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        horizontal_flip=True,  # randomly flip images
+        vertical_flip=False)  # we don't expect vertical flip in this case
+
+# fit parameters from data
+datagen.fit(x_train)
+
+# configure batch size and retrieve one batch of images
+for X_batch, y_batch in datagen.flow(x_train, y_train, batch_size=9):
+    # create a grid of 3x3 images
+    for i in range(0, 9):
+        plt.subplot(330 + 1 + i)
+        plt.imshow(X_batch[i].reshape(28, 28), cmap=plt.get_cmap('gray'))
+    # show the plot
+    plt.show()
+    break
